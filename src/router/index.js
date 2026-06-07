a63163ecb2eeb3cc4f8473c5f5f6f2fe086add79
arthur <engs-arthurkrecskovski@camporeal.edu.br>
2026-06-06 22:50:57 -0300

    criando o index js no router e fazendo imports

---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Dashboard   from '../views/Dashboard.vue'
+import Clientes    from '../views/Clientes.vue'
+import Agendamento from '../views/Agendamento.vue'
+import Historico   from '../views/Historico.vue'
+import Interacao   from '../views/Interacao.vue'
