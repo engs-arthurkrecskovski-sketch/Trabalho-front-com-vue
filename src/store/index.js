6a99e5b0faccfad17473d0a96975963e00ccd0ed
arthur <engs-arthurkrecskovski@camporeal.edu.br>
2026-06-06 23:03:30 -0300

    finalizando o js do store

--- src/store/index.js
+++ src/store/index.js
@@ -3,3 +3,16 @@ import { reactive } from 'vue'
 export const store = reactive({
   clientes: [],
   agendamentos: [],
+
+    adicionarCliente(cliente) {
+    this.clientes.push(cliente)
+  },
+
+  removerCliente(index) {
+    this.clientes.splice(index, 1)
+  },
+
+  adicionarAgendamento(agendamento) {
+    this.agendamentos.push(agendamento)
+  },
+})
