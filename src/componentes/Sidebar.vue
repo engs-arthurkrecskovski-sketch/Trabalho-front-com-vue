c341e5a983464a9083741fb5c3ff4e443a91a1d3
arthur <engs-arthurkrecskovski@camporeal.edu.br>
2026-06-06 22:25:59 -0300

    criando a sidebar

---
+++ src/componentes/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="sidebar">
+    <div class="logo-text-container">
+      <i class="fas fa-tools logo-icon-red"></i>
+      <h2>REAL<span>CARS</span></h2>
+    </div>
+
+    <ul>
+      <li v-for="item in menuItems" :key="item.path">
+        <RouterLink :to="item.path" :class="{ active: route.path === item.path }">
+          <i :class="item.icon"></i> {{ item.label }}
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+
