7bc25e197fcea84d18dcfd061ac6d38eb1f726a7
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-06-08 14:20:59 -0300

    Cria cabecalho de interacao com cliente

---
+++ src/views/Interacao.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div class="interacao-wrapper">
+
+      <div class="header-page">
+        <div class="header-left">
+          <i class="fas fa-comments header-icon"></i>
+          <div>
+            <h1>Interação com Cliente</h1>
+            <p>Envie mensagens, registre feedbacks e acompanhe o atendimento</p>
+          </div>
+        </div>
+        <div class="status-badge">
+          <i class="fas fa-circle"></i> Atendimento Ativo
+        </div>
+      </div>
