408b5da9063763b41524bc22f40d1cf5e6997b12
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-06-08 09:27:11 -0300

    cria a estrutura inicial e cabeçalho da página de histórico

---
+++ src/views/Historico.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <div class="historico-content">
+      <!-- Cabeçalho da página -->
+      <div class="header-page">
+        <i class="fas fa-history header-icon"></i>
+        <h1>Histórico de Serviços</h1>
+        <p>Veja todos os serviços realizados e pendentes</p>
+      </div>
