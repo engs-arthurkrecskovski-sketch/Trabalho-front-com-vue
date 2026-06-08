c2305cf4dc44f18e780243b6ad2ab3f39678ec47
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-06-08 15:10:06 -0300

    Adiciona card de resumo do cliente

--- src/views/Interacao.vue
+++ src/views/Interacao.vue
@@ -72,3 +72,23 @@
             </button>
           </div>
         </div>
+
+        <div class="coluna-info">
+
+          <div class="card-cliente" :class="{ vazio: !clienteSelecionado }">
+            <div v-if="!clienteSelecionado" class="card-placeholder">
+              <i class="fas fa-user-circle"></i>
+              <p>Nenhum cliente selecionado</p>
+            </div>
+            <div v-else>
+              <div class="cliente-avatar">
+                <i class="fas fa-user"></i>
+              </div>
+              <h3>{{ clienteSelecionado.nome }}</h3>
+              <p class="veiculo-label"><i class="fas fa-car"></i> {{ clienteSelecionado.veiculo }}</p>
+              <p class="telefone-label"><i class="fas fa-phone"></i> {{ clienteSelecionado.telefone }}</p>
+              <div class="servico-ativo">
+                <span class="badge-servico">{{ clienteSelecionado.servico }}</span>
+              </div>
+            </div>
+          </div>
