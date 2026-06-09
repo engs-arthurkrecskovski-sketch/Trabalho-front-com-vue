54ecd775c5a95578d5b7c509352e237774034476
Murilo <murilogrando321@gmail.com>
2026-06-09 14:17:00 -0300

    formulario de cadastro e confirmação

--- src/views/Clientes.vue
+++ src/views/Clientes.vue
@@ -1,12 +1,18 @@
 <template>
     <main>
-        <div class="form-container">
+    <div class="form-container">
         <div class="header-content">
             <i class="fas fa-user-plus header-icon"></i>
             <h1>Cadastrar Cliente</h1>
             <p>preencha os dados para iniciar um novo atendimento premium</p>
         </div>
 
-        </div>
+        <ClienteForm @cadastrado="aoCadastrar" />
+      <ClienteModal
+        :modal-aberto="modalAberto"
+        :ultimo-cadastro="ultimoCadastro"
+        @fechar="fecharModal"
+      />
+    </div>
     </main>
 </template>
