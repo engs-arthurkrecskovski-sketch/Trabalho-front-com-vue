dc391f36f44892884992127e80e02c303b0d6aeb
Murilo <murilogrando321@gmail.com>
2026-06-09 19:51:52 -0300

    serviços em destaque e açoes rapidas

---
+++ src/views/Dashboar.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <div class="dashboard-wrapper">
+      <DashboardHeader />
+
+      <DashboardCards />
+
+      <DashboardGrid />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import DashboardHeader from '../components/dashboard/DashboardHeader.vue'
+import DashboardCards from '../components/dashboard/DashboardCards.vue'
+import DashboardGrid from '../components/dashboard/DashboardGrid.vue'
+</script>
+
+<style scoped>
+.dashboard-wrapper {
+  width: min(860px, 100%);
+}
+</style>
