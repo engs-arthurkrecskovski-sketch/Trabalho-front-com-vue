0673bee3b3dd76d90ffee9a792362ca608808fb7
Murilo <murilogrando321@gmail.com>
2026-06-09 14:13:10 -0300

    cabeçalho da pagina

--- src/views/Clientes.vue
+++ src/views/Clientes.vue
@@ -1,7 +1,11 @@
 <template>
     <main>
         <div class="form-container">
-            
+        <div class="header-content">
+            <i class="fas fa-user-plus header-icon"></i>
+            <h1>Cadastrar Cliente</h1>
+            <p>preencha os dados para iniciar um novo atendimento premium</p>
+        </div>
 
         </div>
     </main>
