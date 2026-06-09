beb6d8e32e2d81beb8b2de4b32b7f44af7e3ef79
Murilo <murilogrando321@gmail.com>
2026-06-09 14:44:31 -0300

    renderizacao da lista de clientes e botao para remover cliente da lista

--- src/componentes/ClienteLista.vue
+++ src/componentes/ClienteLista.vue
@@ -10,6 +10,21 @@
       />
     </div>  
 
+        <div
+      v-for="(cliente, index) in clientesFiltrados"
+      :key="index"
+      class="cliente-item"
+    >
+      <div class="cliente-info">
+        <strong>{{ cliente.nome }}</strong>
+        <span>{{ cliente.veiculo }}</span>
+        <span class="tel">{{ cliente.telefone }}</span>
+      </div>
+      <button class="btn-remover" @click="store.removerCliente(index)" title="Remover">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+
 </template>
 
 
