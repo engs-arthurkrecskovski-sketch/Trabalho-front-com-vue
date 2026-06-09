a5529efbdbb00c0ae3af1e2e5b481c17acd5c314
Murilo <murilogrando321@gmail.com>
2026-06-09 14:29:08 -0300

    campo de veiculo e placa

--- src/componentes/ClienteFrom.vue
+++ src/componentes/ClienteFrom.vue
@@ -9,6 +9,17 @@
         :class="{ 'input-erro': erros.nome }"
         @input="limparErro('nome')"
       />
-      <!-- v-if para mostrar mensagem de erro apenas quando necessário -->
       <span v-if="erros.nome" class="msg-erro">{{ erros.nome }}</span>
+    </div>
+
+     <div class="form-group">
+      <label><i class="fas fa-car"></i> Veículo e Placa</label>
+      <input
+        v-model="form.veiculo"
+        type="text"
+        placeholder="Ex: Corolla - ABC1234"
+        :class="{ 'input-erro': erros.veiculo }"
+        @input="limparErro('veiculo')"
+      />
+      <span v-if="erros.veiculo" class="msg-erro">{{ erros.veiculo }}</span>
     </div>
