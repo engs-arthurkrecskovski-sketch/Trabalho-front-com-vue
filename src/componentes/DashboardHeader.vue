fc44d8f38faa6c77c69008dc2e67ed8fc900803d
Murilo <murilogrando321@gmail.com>
2026-06-09 20:07:11 -0300

    cabeçalho do painel

---
+++ src/componentes/DashboardHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header-dashboard">
+    <div class="logo-top-right">
+      <img src="/logo.png" alt="Logo RealCars" />
+    </div>
+    <h1>Painel Geral</h1>
+    <p>Resumo operacional da oficina hoje</p>
+  </div>
+</template>
+
+<script setup>
+</script>
